bad57bb120438983e71e65c0b1dd2eefc0dfbf0e
Harvey C <harveydavid14@gmail.com>
2024-11-26 15:23:21 -0500

    Added json data to login test

--- tests/specs/login-test.spec.ts
+++ tests/specs/login-test.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
+import { readFileSync } from 'fs';
+
+const data = JSON.parse(readFileSync('./tests/data/testData.json', 'utf-8'));
 
 test('Should successfuly log in with a valid user', async ({ page }) => {
   const loginPage = new LoginPage(page, test.info());
   // Expect a title "to contain" a substring.
   await loginPage.openPage();
-  await loginPage.fillUsername("standard_user");
-  await loginPage.fillPassword("secret_sauce");
+  await loginPage.fillUsername(data.users.validUser.username);
+  await loginPage.fillPassword(data.users.validUser.password);
   await loginPage.clickOnLoginBtn();
   await expect(page).toHaveURL(/inventory/);
 });
@@ -15,8 +18,8 @@ test('Should show an error when logging in with invalid user', async ({ page }) 
   const loginPage = new LoginPage(page, test.info());
   // Expect a title "to contain" a substring.
   await loginPage.openPage();
-  await loginPage.fillUsername("locked_out_user");
-  await loginPage.fillPassword("secret_sauce");
+  await loginPage.fillUsername(data.users.invalidUser.username);
+  await loginPage.fillPassword(data.users.invalidUser.password);
   await loginPage.clickOnLoginBtn();
-  expect(await loginPage.getLoginErrorMessage()).toEqual("Epic sadface: Sorry, this user has been locked out.")
+  expect(await loginPage.getLoginErrorMessage()).toEqual(data.loginErrorMessage);
 });
