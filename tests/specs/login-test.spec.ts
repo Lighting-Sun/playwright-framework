880573a32cd455da2f01b0e1813cc3c512ab08ff
Harvey C <harveydavid14@gmail.com>
2024-11-26 13:43:49 -0500

    Added error scenario when logging with invalid user

--- tests/specs/login-test.spec.ts
+++ tests/specs/login-test.spec.ts
@@ -10,3 +10,13 @@ test('Should successfuly log in with a valid user', async ({ page }) => {
   await loginPage.clickOnLoginBtn();
   await expect(page).toHaveURL(/inventory/);
 });
+
+test('Should show an error when logging in with invalid user', async ({ page }) => {
+  const loginPage = new LoginPage(page, test.info());
+  // Expect a title "to contain" a substring.
+  await loginPage.openPage();
+  await loginPage.fillUsername("locked_out_user");
+  await loginPage.fillPassword("secret_sauce");
+  await loginPage.clickOnLoginBtn();
+  expect(await loginPage.getLoginErrorMessage()).toEqual("Epic sadface: Sorry, this user has been locked out.")
+});
