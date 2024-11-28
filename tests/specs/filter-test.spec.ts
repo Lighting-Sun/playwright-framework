3d82b2460aabee3898e199e38476ed0d7a3f5471
Harvey Cortes Roa <39845967+Lighting-Sun@users.noreply.github.com>
2024-11-28 17:19:48 -0500

    Filter test (#8)
    * +Created filter-test.spec file
    +Simplified login.page.ts to reduce report cluttering and added loginWithCredentials method
    
    * Completed Should successfully sort products test case

---
+++ tests/specs/filter-test.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+import { readFileSync } from 'fs';
+import { InventoryPage } from '../pages/inventory.page';
+import UtilsMethods from '../utils/utilsMethods.utils';
+
+test('Should successfully sort products', async ({ page }) => {
+
+    const data = JSON.parse(readFileSync('./tests/data/testData.json', 'utf-8'));
+
+    const loginPage = new LoginPage(page, test.info());
+    const inventoryPage = new InventoryPage(page, test.info());
+
+    await loginPage.openPage();
+    await loginPage.loginWithCredentials(data.users.validUser.username, data.users.validUser.password);
+    await expect(page).toHaveURL(/inventory/);
+    expect(await inventoryPage.header.getPageTitleText()).toEqual('Products');
+    const beforeSortingPrices = UtilsMethods.sortLowToHighValues((await inventoryPage.getTextFromPrices()));
+    await inventoryPage.header.clickOnSortFilterDropdownOption('lohi');
+    const afterSortingPrices = await inventoryPage.getTextFromPrices();
+    expect(beforeSortingPrices).toEqual(afterSortingPrices);
+
+});
