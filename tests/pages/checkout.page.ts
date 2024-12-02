1f24ec2c91f1a0fa31619b06c44bf8b530a0a429
Harvey C <harveydavid14@gmail.com>
2024-12-02 17:40:51 -0500

    Added locators to checkout page

--- tests/pages/checkout.page.ts
+++ tests/pages/checkout.page.ts
@@ -10,5 +10,23 @@ export class CheckoutPage extends BasePage {
 
     header = new Header(this._page, this._testInfo);
 
+    locators = {
+        firstNameInput: {
+            selector: "#first-name",
+            description: "first name input",
+        },
+        lastNameInput: {
+            selector: "#last-name",
+            description: "last name input",
+        },
+        postalCodeInput: {
+            selector: "#postal-code",
+            description: "postal code input",
+        },
+        continueButton: {
+            selector: "#continue",
+            description: "continue button",
+        },
+    };
 
 }
